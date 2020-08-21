83fb563b63e2a93521e539f954f0d9b62610083a
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2020-08-21 12:39:16 +0500

    Don't export Logger mock alongside with other exports

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -2,4 +2,3 @@ export { InjectLogger } from './inject-logger.decorator';
 export { LogMethodCall } from './log-method-call.decorator';
 export { LoggerModule } from './logger.module';
 export { LoggerService } from './logger.service';
-export { LoggerServiceMock } from './logger.service.mock';
